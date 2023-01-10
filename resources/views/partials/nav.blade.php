9cdf2d3cd0168d9923ac635e7dd54355964fd811
SaverioCascelli <saverio.cascelli@gmail.com>
2023-01-10 16:23:24 +0100

    init

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="{{ route('home') }}">home</a>
+            </li>
+            <li><a href="{{ route('trains') }}">trains</a>
+            </li>
+        </ul>
+    </nav>
+</header>
